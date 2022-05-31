f287a6442391b9eb946ab6bd253721ac7f699c8f
kykim0613 <97214073+kykim0613@users.noreply.github.com>
2022-05-31 16:41:32 +0900

    practice socket

---
+++ src/server.js
@@ -0,0 +1,72 @@
+import express from 'express';
+import http from 'http';
+import { Server } from 'socket.io';
+
+const app = express();
+
+app.set("view engine", "pug");
+app.set("views", __dirname + '/views');
+app.use("/public", express.static(__dirname + '/public'))
+app.get('/', (_, res) => res.render('home'));
+app.get('/*', (_, res) => res.redirect('/'));
+
+const handleListen = () => console.log(`Listen on http://localhost:3000`);
+
+const httpServer = http.createServer(app);
+const wsServer = new Server(httpServer);
+
+
+wsServer.on("connection", socket => {
+    socket["nickname"] = "Yunki";
+    socket.onAny((event) => {
+        console.log((`Socket Event: ${event}`))
+    });
+    socket.on("enter_room", (roomName, done) => {
+        socket.join(roomName);
+        done()
+        socket.to(roomName).emit("welcome!", socket.nickname);
+    });
+    socket.on("disconnecting", () => {
+        socket.rooms.forEach(room => socket.to(room).emit("bye", socket.nickname));
+    })
+    socket.on("new_message", (msg, room, done) => {
+        socket.to(room).emit("new_message", `${socket.nickname}: ${msg}`);
+        done();
+    })
+    socket.on("nickname", nickname => socket["nickname"] = nickname)
+})
+
+httpServer.listen(3000, handleListen);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const sockets = [];
+
+// wss.on("connection", (socket) => {
+//     sockets.push(socket);
+//     socket["nickname"] = "Yunki";
+//     console.log("Connected to Browser");
+//     socket.on("close", () => console.log("Disconnected Client"))
+//     socket.on("message", (msg) => {
+//         const message = JSON.parse(msg);
+//         switch(message.type) {
+//             case "new_message":
+//                 sockets.forEach(aSocket => 
+//                     aSocket.send(`${socket.nickname}: ${message.payload}`));
+//             case "nickname":
+//                 socket["nickname"] = message.payload;
+//         }
+
+//     })
+// })
